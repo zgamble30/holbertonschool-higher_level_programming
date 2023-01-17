d18ffd6254761d500b8af782d6e21e17398318b8
zgamble30 <you@example.com>
2023-01-17 10:16:54 -0800

    fix bug in task 0

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    """Compute the square value of all integers of a matrix."""
-    return ([list(map(lambda x: x * x, row)) for row in matrix])
+    mtx = [[x**2 for x in num] for num in matrix]
+
+    return mtx
