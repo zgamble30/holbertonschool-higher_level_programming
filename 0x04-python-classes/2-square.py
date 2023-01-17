4f1a46911ae546becb5f530763472be86ba130f2
zgamble30 <you@example.com>
2023-01-17 10:54:25 -0800

    function for area of a square

---
+++ 0x04-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Class Sqaure based on size"""
+
+
+class Square:
+    """Class Square based on size"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
