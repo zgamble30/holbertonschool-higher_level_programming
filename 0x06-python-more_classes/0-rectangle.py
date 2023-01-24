76418f78fd8692a6a4c53f72be7b5a8f0344789f
zgamble30 <you@example.com>
2023-01-24 12:54:30 -0800

    added specs to class rectangle

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -1,7 +1,40 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""Rectangle class that defines a rectangle"""
 
 
 class Rectangle:
-    """Represent a rectangle."""
-    pass
+    """Passing initializing values to object"""
+    def __init__(self, width=0, height=0):
+
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    @property
+    def width(self):
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
