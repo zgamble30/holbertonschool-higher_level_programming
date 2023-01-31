bc4989dd89300b28494ff4e5b4c9d64f8bfe1fd6
zgamble30 <you@example.com>
2023-01-31 13:20:16 -0800

    updated task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-""" 7-add_item.py
+"""Module 7-add_item with function add_item"""
 
-    adds all arguments to a Python list, and then save them to a file
-"""
+
+import json
 import sys
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-if __name__ == "__main__":
-    filename = "add_item.json"
+size = len(sys.argv)
+with open("add_item.json", 'a+') as f:
+    f.seek(0)
+    read_data = f.read()
+    if read_data == "":
+        json.dump([], f)
 
-    try:
-        items = load_from_json_file(filename)
-    except FileNotFoundError:
-        items = []
-
-    for item in sys.argv[1::]:
-        items.append(item)
-
-    save_to_json_file(items, filename)
+new_list = load_from_json_file("add_item.json")
+for i in range(1, size):
+    new_list.append(sys.argv[i])
+save_to_json_file(new_list, "add_item.json")
