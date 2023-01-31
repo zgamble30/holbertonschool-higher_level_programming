3d40b86a16d142e7560ef1df86a827600b4f412c
zgamble30 <you@example.com>
2023-01-31 12:56:35 -0800

    added a function that writes an Object to a text file, using a JSON representation

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,19 @@
+i#!/usr/bin/python3
+""" 7-save_to_json_file.py
+
+    save_to_json_file func
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save_to_json_file func
+
+    writes an Object to a text file, using a JSON representation
+
+    Args:
+        my_obj (object): object
+        filename (FILE): file
+    """
+    with open(file=filename, mode="w") as f:
+        f.write(json.dumps(my_obj))
