aff1e6eb2bf995399915e052659a49ed20ef35e6
zgamble30 <you@example.com>
2023-01-31 13:25:09 -0800

    updated bugs on task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""Module 7-add_item with function add_item"""
-
-
-import json
+"""# 7-add_item.py"""
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import json
+
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
 
 
-size = len(sys.argv)
-with open("add_item.json", 'a+') as f:
-    f.seek(0)
-    read_data = f.read()
-    if read_data == "":
-        json.dump([], f)
+def add_items():
+    items = []
+    for item in sys.argv[1:]:
+        items.append(item)
 
-new_list = load_from_json_file("add_item.json")
-for i in range(1, size):
-    new_list.append(sys.argv[i])
-save_to_json_file(new_list, "add_item.json")
+    with open("add_item.json", "w") as f:
+        json.dump(items, f)
+
+if __name__ == "__main__":
+    add_items()
