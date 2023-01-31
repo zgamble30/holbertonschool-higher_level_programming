182de4c7e797e6843a28f9598a23e2b837fffc57
zgamble30 <you@example.com>
2023-01-31 13:04:06 -0800

    bug fixes on task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,19 +1,11 @@
-i#!/usr/bin/python3
-"""5-save_to_json_file.py
+#!/usr/bin/python3
+# 5-save_to_json_file.py
 
-    save_to_json_file func
-"""
+"""Defines a JSON file-writing function."""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """save_to_json_file func
-
-    writes an Object to a text file, using a JSON representation
-
-    Args:
-        my_obj (object): object
-        filename (FILE): file
-    """
-    with open(file=filename, mode="w") as f:
-        f.write(json.dumps(my_obj))
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
