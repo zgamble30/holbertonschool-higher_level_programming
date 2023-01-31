3b1b7758fe27e474a3a918a4804a39b86bc54277
zgamble30 <you@example.com>
2023-01-31 12:40:12 -0800

    added a function that reads a text file (UTF8) and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" 0-read_file.py
+
+    read_file func
+"""
+
+
+def read_file(filename=""):
+    """read_file func
+
+    reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename (str, optional): file. Defaults to "".
+    """
+    with open(file=filename, mode="r", encoding="utf8") as f:
+        for line in f:
+            print(line, end="")
