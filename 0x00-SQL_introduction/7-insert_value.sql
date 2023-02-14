d8465ff7252e8b6aa60a241359cc89e71164ea09
zgamble30 <you@example.com>
2023-02-14 12:24:24 -0800

    fixed bugs on task 7

--- 0x00-SQL_introduction/7-insert_value.sql
+++ 0x00-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- inserts a new row in the table first_table
--- (database hbtn_0c_0) in your MySQL server.
+-- Inserts a new row into the table first_table.
 INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
