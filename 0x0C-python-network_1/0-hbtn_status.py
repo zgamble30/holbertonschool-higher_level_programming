84c3230de4ce13f1e081a8bdfdccb67581da51a1
zgamble30 <you@example.com>
2023-03-14 14:23:55 -0700

    trying to fix bug with output

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-""" 0-hbtn_status
-
-    Fetches https://intranet.hbtn.io/status
-"""
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
 
-def main():
-    """Check the status of URL
-    """
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {:}".format(type(html)))
-        print("\t- content: {:}".format(html))
-        print("\t- utf8 content: {:}".format(html.decode("UTF-8")))
-
-
 if __name__ == "__main__":
-    main()
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
