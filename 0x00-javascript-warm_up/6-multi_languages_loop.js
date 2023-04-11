bffddfe3bbedc62a1003e3ad407730b94a0eca54
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-04-11 02:00:09 -0500

    Update 6-multi_languages_loop.js

--- 0x00-javascript-warm_up/6-multi_languages_loop.js
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-// Prints 3 lines: (like 1-multi_languages.js)
-// but by using an array of string and a loop
-const message = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const value of message) {
-  console.log(value);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
 }
