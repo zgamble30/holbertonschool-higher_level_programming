13c032854ba511193ddcaf9fd82fe131c4540b3b
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-04-11 02:06:58 -0500

    Update 6-multi_languages_loop.js

--- 0x00-javascript-warm_up/6-multi_languages_loop.js
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
 }
