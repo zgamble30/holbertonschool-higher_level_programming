8adf4ff8b8253c0b844eb7986284cfd0f7bb0409
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-04-17 16:22:07 -0500

    Create 2-script.js

---
+++ 0x03-javascript-web_jquery/2-script.js
@@ -0,0 +1,3 @@
+// Updates the text color of the HTML tag HEADER to red (#FF0000)
+// when the user clicks on the tag DIV#red_header
+$('DIV#red_header').click(() => $('HEADER').css('color', '#FF0000'));
