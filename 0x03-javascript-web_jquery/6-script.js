030000859176124f3e7655bef2cbe01264575042
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-04-17 16:24:42 -0500

    Create 6-script.js

---
+++ 0x03-javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+// Updates the text of the HTML tag HEADER to “New Header!!!”
+// when the user clicks on DIV#update_header
+$('DIV#update_header').click(() => {
+  $('HEADER').text('New Header!!!');
+});
